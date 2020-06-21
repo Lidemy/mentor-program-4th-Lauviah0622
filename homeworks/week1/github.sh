1aba1019ef4ddadd0a7db16b2186fff10a9a3e3f
Lauviah Fang <lauviah0622@gmail.com>
2020-06-22 00:19:23 +0800

    super-challenge update

--- homeworks/week1/github.sh
+++ homeworks/week1/github.sh
@@ -6,7 +6,6 @@ export PATH
 #1. 讓 wget 可以使用 pipe 來利用指令
 #2. 可以 get 到 特定 pattern
 #3. 儲存成檔案
-asd="123"
 rep=$(wget -qO- https://api.github.com/users/${1})
 
 
@@ -14,8 +13,3 @@ for info in "name" "bio" "location" "blog"
 do
     echo "${rep}" | grep ${info} | awk 'BEGIN { FS = "\"" } ; {print $4}'
 done
-
-#${rep} | grep "bio"
-#wget -qO- https://api.github.com/users/${1}
-#echo "${a}" 
-#| grep "bio" | awk 'BEGIN { FS = "\"" } ; {print $4}'
