844ecf498ab95a98365acad032f8f948cf306c1a
Lauviah0622 <lauviah0622@gmail.com>
2020-07-12 00:06:30 +0800

    check eslint, update hw2/js

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -2,9 +2,21 @@ const https = require('https');
 
 const base = 'https://lidemy-book-store.herokuapp.com';
 const parsedBody = buffer => JSON.parse(buffer.toString('utf-8'));
+const getHeader = (method, path, data) => (
+  {
+    hostname: 'lidemy-book-store.herokuapp.com',
+    port: 443,
+    path,
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(data),
+    },
+  }
+);
 
-function list() {
-  https.get(`${base}/books?_limit=20`, (res) => {
+function list(num) {
+  https.get(`${base}/books?_limit=${num}`, (res) => {
     res.on('data', (buffer) => {
       const rawData = parsedBody(buffer);
       const bookList = rawData.map(e => `${e.id}: ${e.name}`);
@@ -17,17 +29,16 @@ function list() {
 
 function read(id) {
   https.get(`${base}/books/${id}`, (res) => {
+    console.log(res.statusCode);
     res.on('data', (buffer) => {
       const rawData = parsedBody(buffer);
-      if (rawData.prototype.hasOwnProperty.call(undefined, 'name')) {
-        console.log(`${rawData.id}: ${rawData.name}`);
-        return Promise.resolve(id);
+      if (rawData.hasOwnProperty('name')) { //eslint-disable-line
+        console.log(`#${rawData.id}: ${rawData.name}`);
+      } else {
+        console.log(`can't found the book #${id}`);
       }
-      console.log('can\'t found the book');
-      return Promise.reject(id);
     });
   }).on('error', (e) => {
-    console.log('read Error');
     console.log(e);
   });
 }
@@ -64,33 +75,22 @@ function create(name) {
     name,
   });
 
-  const options = {
-    hostname: 'lidemy-book-store.herokuapp.com',
-    port: 443,
-    path,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length,
-    },
-  };
-
+  const options = getHeader('POST', path, data);
 
   const req = https.request(options, (res) => {
-    // if (res.statusCode > 400) {
+    if (res.statusCode > 400) {
+      console.log('something getting wrong in adding new book');
+    }
 
-    // }
+    if (res.statusCode >= 200 && res.statusCode < 400) {
+      // console.log('statusCode:', res.statusCode);
 
-    // if (res.statusCode >= 200 && res.statusCode < 400) {
-    // }
-    console.log('statusCode:', res.statusCode);
-
-    res.on('data', (buffer) => {
-      const rawData = parsedBody(buffer);
-      console.log(rawData);
-    });
+      res.on('data', (buffer) => {
+        const rawData = parsedBody(buffer);
+        console.log(`add book success！ Your book "${rawData.name}" is #${rawData.id}`);
+      });
+    }
   });
-
 
   req.on('error', (e) => {
     console.error(e);
@@ -105,21 +105,10 @@ function update(id, name) {
     name,
   });
 
-  const options = {
-    hostname: 'lidemy-book-store.herokuapp.com',
-    port: 443,
-    path,
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length,
-    },
-  };
-
+  const options = getHeader('PATCH', path, data);
   const req = https.request(options, (res) => {
     console.log('statusCode:', res.statusCode);
   });
-
 
   req.on('error', (e) => {
     console.error(e);
@@ -131,20 +120,26 @@ function update(id, name) {
 
 (function entry() {
   const operation = process.argv[2];
-  const args = process.argv.slice(3);
+  const args = process.argv.slice(3, 4);
   if (operation === 'list') {
-    list(args);
-  }
-  if (operation === 'read') {
+    list(args.length === 0 ? 20 : args);
+  } else if (operation === 'read') {
     read(args[0]);
-  }
-  if (operation === 'delete') {
+  } else if (operation === 'delete') {
     deleteBook(args[0]);
-  }
-  if (operation === 'create') {
+  } else if (operation === 'create') {
     create(...args);
-  }
-  if (operation === 'update') {
+  } else if (operation === 'update') {
     update(...args);
+  } else {
+    console.log(`
+you command get something wrong, following info can help you
+
+print book list   : node <filename> list
+print single book : node <filename> read <:id>
+delete book       : node <filename> delete <:id>
+create new book   : node <filename> create <:new book name>
+update book name  : node <filename> update <:id> <:new name>
+`);
   }
 }());
